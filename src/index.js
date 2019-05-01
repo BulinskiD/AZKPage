875a5c99a2c9f4edd43db7f54213e0b686463ec8
BulinskiD <dawidbulinski132@gmail.com>
2019-05-01 14:34:37 +0200

    Added connection to firebase with firebase SDK and React Context

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import firebase from './api/firebase';
+import FirebaseContext from './api/firebaseContext';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+    <FirebaseContext.Provider value = {firebase.firestore()}>
+            <App />
+    </FirebaseContext.Provider>
+, document.querySelector('#root'));
