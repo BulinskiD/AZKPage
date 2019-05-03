b56a4ca93753cb7e4df07cfdbbbd28fa908611dd
BulinskiD <dawidbulinski132@gmail.com>
2019-05-03 15:31:10 +0200

    Added CRUD operations for styles firestore and storage support for uploading images

--- src/components/adminApp/styles/index.js
+++ src/components/adminApp/styles/index.js
@@ -1,118 +1,112 @@
 import React from 'react';
-import updatePic from '../../../helperFunctions/updatePic';
 import onErrorHandler from '../../../helperFunctions/onErrorHandler';
-import styleApi from '../../../api/styles';
+import FirebaseContext from '../../../api/firebaseContext';
 
 import ModalForm from '../modalForm'; 
 
 class StylesAdmin extends React.Component{
-
-    state= {styles: null, name: '', picSrc: '', styleID: null, error: null};
+    static contextType = FirebaseContext;
+    
+    storageRef = this.context.storage;
+    styleRef = this.context.firestore.collection('styles');
+    state= {styles: null, name: '', src: '', id: null, error: null};
 
     errors = {name: null};
 
-
-
-    componentDidMount(){
-        styleApi.get('index/')
-        .then((response)=>{
-            this.setState({styles: response.data});
-        })
-        .catch(error=>{
-            let verifiedError = onErrorHandler(error);
-            if(verifiedError === 404){
-                this.setState({error: 'Brak dodanych styli'});
-            }
-        });
-    }
-
-
-
-    deleteStyle(style){
-        styleApi.delete(`delete/${style.styleID}`,  { headers: {'Authorization': "bearer " + localStorage.getItem('jwt')}})
-            .then((response)=>{
-                let newArr= this.state.styles.filter(oldStyle=> oldStyle.styleID!==style.styleID);
-                let error = newArr.length === 0 ? 'Brak dodanych styli' : null;
-                this.setState({styles: newArr, error: error});
-            }).catch((error)=>{
-                let verifiedError = onErrorHandler(error);
-                if(verifiedError === 404){
-                    // Not found Style
-                }
+    async componentDidMount() {
+        try {
+            let styleNames = await this.styleRef.get();
+            styleNames = styleNames.docs.map(async style => {
+                let newUrl = await this.storageRef.child(style.data().src).getDownloadURL();
+                return { id: style.id, url: newUrl, ...style.data() }
             });
-
+            //Await for all promises to be completed, then set state
+            Promise.all(styleNames).then(completed => {
+                this.setState({ styles: completed });
+            });
+        }
+        catch (error) {
+            //TO DO CHANGE ERROR HANDLING!!
+        }
     }
 
-    editStyle= async(e) =>{
-        let editedStyle = { name: this.state.name, picSrc: this.state.picSrc, styleID: this.state.styleID };
-        if (document.getElementById('picture').files[0] !== undefined) {
-            let file = document.getElementById('picture').files[0];
-            if (file.name !== this.state.picSrc) {
-                editedStyle.picSrc = `/img/styles/${file.name}`;
-                await updatePic(file, 'styles', localStorage.getItem('jwt'));
-            }
+    deleteStyle = async style => {
+        try {
+        await this.styleRef.doc(style.id).delete();
+        let newArr= this.state.styles.filter(oldStyle=> oldStyle.id!==style.id);
+        await this.storageRef.child(style.src).delete();
+        let error = newArr.length === 0 ? 'Brak dodanych styli' : null;
+        this.setState({styles: newArr, error: error});
+        }
+        catch(error) {
+
         }
 
-        styleApi.put(`update/${this.state.styleID}`, editedStyle,  { headers: {'Authorization': "bearer " + localStorage.getItem('jwt')}})
-            .then((response) => {
-                let index = this.state.styles.findIndex((style) => style.styleID === editedStyle.styleID);
-                let updatedArr = this.state.styles;
-                updatedArr[index] = editedStyle;
-                this.setState({ styles: updatedArr });
-                document.getElementById('cancel').click();
-                document.getElementById('picture').value= '';
-            }).catch((error) => {
-                let verifiedError = onErrorHandler(error);
-                if(verifiedError === 400){
-                    //Error on validation
-                }
-                if(verifiedError === 404){
-                    //Not found style
-                }
-            });
-
     }
 
-    addStyle= async(e)=>{
+    editStyle = async () =>{
+        let editedStyle = { name: this.state.name, src: this.state.src };
+        try{
+        if (document.getElementById('picture').files[0] !== undefined) {
+            let file = document.getElementById('picture').files[0];
+            if (file.name !== this.state.src) {
+                let fileRef = this.storageRef.child(file.name);
+                editedStyle.src = `/img/styles/${file.name}`;
+                await fileRef.put(file);
+            }
+        }
+        await this.styleRef.doc(this.state.id).update(editedStyle);
+        editedStyle.url = await this.storageRef.child(editedStyle.src).getDownloadURL();
+        editedStyle.id = this.state.id;
+        let index = this.state.styles.findIndex((style) => style.id === this.state.id);
+        let updatedArr = this.state.styles;
+        updatedArr[index] = editedStyle;
+        this.setState({ styles: updatedArr });
+        document.getElementById('cancel').click();
+        document.getElementById('picture').value= '';
+        }
+        catch(error) {
+            console.log(error);
+        }
+    }
+
+    addStyle = async () =>{
         let file = document.getElementById('picture').files[0];
-        await updatePic(file, 'styles', localStorage.getItem('jwt'));
-
-        let newStyle = { name: this.state.name, picSrc: `/img/styles/${file.name}` }
-
-        styleApi.post(`add/`, newStyle,  { headers: {'Authorization': "bearer " + localStorage.getItem('jwt')}})
-            .then((response) => {
-                let updatedArr = this.state.styles;
-                updatedArr.push(newStyle);
-                this.setState({ styles: updatedArr });
-                document.getElementById('cancel').click();
-                document.getElementById('picture').value= '';
-            }).catch((error) => {
-                let verifiedError = onErrorHandler(error);
-                if(verifiedError === 400){
-                    //Error on validation
-                }
-            });
+        let fileRef = this.storageRef.child(file.name);
+        try{
+            await fileRef.put(file);
+            let newStyle = { name: this.state.name, src: `${file.name}` }
+            let docRef = await this.styleRef.add(newStyle);
+            newStyle.id = docRef.id;
+            newStyle.url = await this.storageRef.child(newStyle.src).getDownloadURL();
+            let updatedArr = this.state.styles;
+            updatedArr.push(newStyle);
+            this.setState({ styles: updatedArr });
+            document.getElementById('cancel').click();
+            document.getElementById('picture').value= '';
+        }
+        catch(error) {
+            console.log(error);
+        }
     }
-
 
     openBlankModal=()=>{
-        this.setState({name: '', pic: '', styleID: null});
+        this.setState({name: '', src: '', id: null});
         document.getElementById('openModal').click();
     }
 
     openEditModal=(style)=>{
-        this.setState({name: style.name, picSrc: style.picSrc, styleID: style.styleID});
+        this.setState({name: style.name, src: style.src, id: style.id});
         document.getElementById('openModal').click();
     }
-
 
     renderStylesList(){
         if(this.state.styles !==null){
             return this.state.styles.map(style=>{
                 return (
-                    <div className="paint" style={{cursor: 'default'}} key={style.styleID}>
+                    <div className="paint" style={{cursor: 'default'}} key={style.id}>
                         <div className='thumb'>
-                            <img className="img-thumbnail" src={style.picSrc} alt={style.name} />
+                            <img className="img-thumbnail" src={style.url} alt={style.name} />
                         </div>
                         <h4>{style.name}</h4>
                         <span className="exh-buttons">
@@ -136,7 +130,7 @@ class StylesAdmin extends React.Component{
         <>
             <div className="form-group row">
                 <label htmlFor="name">Nazwa</label>
-                <input  className="form-control" onChange={(e)=>{ e.target.value.length >3 && e.target.value.length < 15 ? this.errors.name = true: this.errors.name = false;
+                <input  className="form-control" onChange={(e)=>{ e.target.value.length < 3 || e.target.value.length > 15 ? this.errors.name = true: this.errors.name = false;
                                                                 this.setState({name: e.target.value})}} 
                                                                 value={this.state.name} type='text' name='name' />
             </div>
@@ -149,7 +143,7 @@ class StylesAdmin extends React.Component{
 
         return (
         <ModalForm title={'Zarządzaj technikami'} 
-                   id={this.state.styleID} 
+                   id={this.state.id} 
                    add={this.addStyle} 
                    edit={this.editStyle} 
                    openBlankModal={this.openBlankModal}
@@ -158,10 +152,6 @@ class StylesAdmin extends React.Component{
             {form}
         </ModalForm>);
     }
-
-
-
-
 
     render(){
         return(
